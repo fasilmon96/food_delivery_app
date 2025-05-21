30c75c5bfa3b2af2893b85e79a7001fe566f9beb
HP <fasilrahman96@gmail.com>
2025-05-21 14:30:05 +0530

    Initial commit

--- lib/core/constant/service.dart
+++ lib/core/constant/service.dart
@@ -1,7 +1,7 @@
    String publishableKey =
-       "pk_test_51REl4IDGAMmpiIeBiWyD45K6uejOVICLdnXy47xdaAQg7sW63VGo4Zeegrptcw2Jh8YKdq9Gu8v0VDAFjNqV0sLf00EEM3jR72";
+       "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
-   String secretKey = "sk_test_51REl4IDGAMmpiIeBQuroFaqhA1wq34BwZHrURKUvmbMmxUMCeNJIBBTNWaBn0SBaE0sKRG0Wn7ipiCQPsuIeCDZO00Ddif9I6a";
+   String secretKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
 
 
